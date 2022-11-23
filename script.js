f4541daab3ac5f8c4d3e65586729b53df3e94652
suppysup <109565166+suppysup@users.noreply.github.com>
2022-11-22 23:56:46 -0600

    adding css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map;
 function initMap() {
-  
-}
+  var map = new google.maps.Map(document.getElementById('map'), {center: {lat: 41.8781, lng: 87.6298}, zoom: 8,})
+};
+window.initMap = initMap;
